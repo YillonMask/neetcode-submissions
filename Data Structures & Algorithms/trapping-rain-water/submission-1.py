25d15cb705c12cd622e276cc78291aa8f26b0e97
Xinrui Yi <77310903+YillonMask@users.noreply.github.com>
2026-07-16 15:12:42 -0700

    Bulk sync: 22 submissions

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        # prefix and suffix array to keep track leftmax and rightmax
+        n = len(height)
+        prefix = [height[0]] * n
+        suffix = [height[n - 1]] * n
+        for i in range(1, n):
+            prefix[i] = max(prefix[i - 1], height[i])
+        for i in range(n - 2, -1, -1):
+            suffix[i] = max(suffix[i + 1], height[i])
+        
+        res = 0
+        for i in range(n):
+            res += min(prefix[i], suffix[i]) - height[i]
+        
+        return res
