df50152bb15c8e91ee6c316d37491f2c6e1bd931
Xinrui Yi <77310903+YillonMask@users.noreply.github.com>
2026-09-08 15:58:59 -0700

    Add: climbing-stairs - submission-3

---
+++ Data Structures & Algorithms/climbing-stairs/submission-3.py
@@ -0,0 +1,13 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        # f(n) = f(n - 1) + f(n - 2)
+        if n <= 2:
+            return n
+        pre = 2
+        prePre = 1
+        
+        for _ in range(3, n + 1):
+            one = pre + prePre
+            prePre = pre
+            pre = one
+        return one
