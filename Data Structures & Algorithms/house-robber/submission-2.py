6d8864d847f25988d84a442ca138d67f6af23d38
Xinrui Yi <77310903+YillonMask@users.noreply.github.com>
2026-09-08 16:24:33 -0700

    Add: house-robber - submission-2

---
+++ Data Structures & Algorithms/house-robber/submission-2.py
@@ -0,0 +1,17 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        # dp[i] -> maximum money from ith room
+        # dp[i] = max(nums[i] + dp[i - 2], dp[i - 1] )
+        
+        n = len(nums)
+        if n <= 1:
+            return nums[0]
+        dp = [-1] * (n)
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+        total = dp[1]
+        for i in range(2, len(nums)):
+            dp[i] = max(nums[i] + dp[i - 2], dp[i - 1])
+            total = max(total, dp[i])
+        
+        return total
